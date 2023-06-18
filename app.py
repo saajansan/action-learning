a7722f35770670787e665e509ca730a4ad993ce0
saajan <designersaajan@gmail.com>
2023-06-18 15:34:40 +0200

    changes size of the pdf, embedded extension removal adding import os and magic for the pdf embeded extension remover

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import magic
 
 app = Flask(__name__)
 
@@ -14,8 +15,27 @@ def upload():
         # Get the uploaded file from the form
         pdf_file = request.files['pdf_file']
 
+        # Validate file extension
+        if not pdf_file.filename.endswith('.pdf'):
+            return render_template('upload.html', error='Invalid file format. Please upload a PDF file.')
+
+        # Validate file size
+        max_size = 5 * 1024 * 1024  # 5 MB
+        if len(pdf_file.read()) > max_size:
+            return render_template('upload.html', error='File size exceeds the limit. Please upload a file up to 5 MB.')
+
+        # Reset file cursor to the beginning for saving
+        pdf_file.seek(0)
+
+        # Validate file content using magic module
+        file_content = pdf_file.read()
+        mime_type = magic.from_buffer(file_content, mime=True)
+        if mime_type != 'application/pdf':
+            return render_template('upload.html', error='Invalid file format. Please upload a valid PDF file.')
+
         # Save the file to a specific folder
         upload_folder = './uploads'  # Replace with the desired folder path
+        pdf_file.seek(0)
         pdf_file.save(os.path.join(upload_folder, pdf_file.filename))
 
         # Redirect to a success page or perform further processing
